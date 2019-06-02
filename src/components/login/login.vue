a31c8744cf32f1d5d8b1e5a0ba768f1eb5b002d1
ligangtao <2860353139@qq.com>
2019-06-02 22:36:54 +0800

    完成数据渲染&新增用户功能

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="loginbox">
     <!-- 
-       el-form: 这个组件中的内容都是表单元素
-           model: 绑定属性
-           status-icon：状态图标
-           :rules: 添加验证规则
-           ref: 在 vue 中可以通过 this.$refs 来操作 dom 元素
-           abel-width：文本的宽度
-           label-positio: 设置表单元素的描述文件顶部对齐
-       el-form-item: 表单中的表单元素
-           label: 当前元素显示的文本
-           prop: 设置验证规则
-       el-input：input 元素
-           type：类型
-           v-model：双向绑定的数据
-           autocomplete：自动补全 （h5）
+         el-form: 这个组件中的内容都是表单元素
+             model: 绑定属性
+             status-icon：状态图标
+             :rules: 添加验证规则
+             ref: 在 vue 中可以通过 this.$refs 来操作 dom 元素
+             abel-width：文本的宽度
+             label-positio: 设置表单元素的描述文件顶部对齐
+         el-form-item: 表单中的表单元素
+             label: 当前元素显示的文本
+             prop: 设置验证规则
+         el-input：input 元素
+             type：类型
+             v-model：双向绑定的数据
+             autocomplete：自动补全 （h5）
     -->
     <el-form
       label-position="top"
@@ -38,6 +38,7 @@
     </el-form>
   </div>
 </template>
+
 <script>
 export default {
   data() {
@@ -50,6 +51,7 @@ export default {
       // 验证规则
       rules: {
         username: [
+          // 这个属性是必须的，如果没有提示 message， 当失去焦点时触发
           { required: true, message: "请输入用户名", trigger: "blur" }
         ],
         password: [{ required: true, message: "请输入密码", trigger: "blur" }]
@@ -58,29 +60,32 @@ export default {
   },
   methods: {
     login() {
-      //判断 form 中的元素是否满足验证条件
+      // 判断 form 中的元素是否满足验证条件
       this.$refs.ruleForm.validate(valid => {
+        // valid boole 值，如果通过返回 true， 否则返回 false
         if (valid) {
-          //  发送登录请求
+          // 发送登录请求
           this.$http.post("login", this.ruleForm).then(res => {
-            // 结构参数
+            // 解构参数
             console.log(res);
             let { data, meta } = res.data;
-            //判断登录状态
-            if (meta.status == 200) {
-              //提示登录成功
+            // 判断登录状态
+            if (meta.status === 200) {
+              // 提示登录成功
               this.$message({
                 message: meta.msg,
                 type: "success"
               });
-              //跳转到首页
-              this.$router.push('/home')
+              // 跳转到首页
+              this.$router.push("/home");
+              // 将 登录 中的 token 保存到浏览器中的 localstorage 中
+              localStorage.setItem("token", res.data.data.token);
             } else {
               this.$message.error(meta.msg);
             }
           });
         } else {
-          this.$message.error("请输入正确的用户和密码");
+          this.$message.error("请输入正确的用户名和密码");
           return false;
         }
       });
@@ -88,6 +93,7 @@ export default {
   }
 };
 </script>
+
 <style scoped>
 .loginbox {
   padding: 40px;
@@ -99,6 +105,7 @@ export default {
   background-color: #fff;
   border-radius: 10px;
 }
+
 .loginbox .mybtn {
   width: 100%;
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-//引入 userElement 组件
-import UserElement from '../components/useElement.vue'
+// 引入 userElement 组件
+import UserElement from '../components/userElement.vue'
 import Login from '../components/login/login.vue'
 import Home from '../components/home/home.vue'
+import Users from '../components/users/users.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    //当请求根目录 重定向到 /home 中
-    // { path: '/', redirect: '/home' }, // 重定向
-    { path: '/', redirect: { name: 'home' } }, // 重定向的目标也可以是一个命名的路由
-    { path: '/userelement', component: UserElement }, // 使用element-ui的路由
+    // 当请求 根目录 重定向到 /home 中
+    // { path: '/', redirect:  '/home'}, // 重定向
+    { path: '/', redirect: { name: 'home' } },
+    { path: '/userelement', component: UserElement }, // 使用 elementui 的路由
     { path: '/login', name: 'login', component: Login }, // 登录的路由
-    { path: '/home', name: 'home', component: Home }
+    // 给 home 添加子路由
+    {
+      path: '/home', name: 'home', component: Home,
+      children: [
+        { path: '/users', name: 'users', component: Users } // 添加一个 home 的子路由 userss
+      ]
+    } // home 的路由
   ]
 })
